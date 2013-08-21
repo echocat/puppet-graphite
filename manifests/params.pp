a1e8a1fd78659792ef4f79a5ce7c09e196b978b6
Daniel Werdermann <dwerdermann@web.de>
2013-08-21 23:29:35 +0200

    RPM name in EPEL for django is Django14. close #12

--- manifests/params.pp
+++ manifests/params.pp
@@ -61,7 +61,7 @@ class graphite::params {
 
   $graphitepkgs = $::osfamily ? {
     debian => ["python-cairo","python-twisted","python-django","python-django-tagging","python-ldap","python-memcache","python-sqlite","python-simplejson"],
-    redhat => ["pycairo", "Django", "python-ldap", "python-memcached", "python-sqlite2",  "bitmap", "bitmap-fonts-compat", "python-devel", "python-crypto", "pyOpenSSL", "gcc", "python-zope-filesystem", "python-zope-interface", "git", "gcc-c++", "zlib-static", "MySQL-python"],
+    redhat => ["pycairo", "Django14", "python-ldap", "python-memcached", "python-sqlite2",  "bitmap", "bitmap-fonts-compat", "python-devel", "python-crypto", "pyOpenSSL", "gcc", "python-zope-filesystem", "python-zope-interface", "git", "gcc-c++", "zlib-static", "MySQL-python"],
   }
 
 }
