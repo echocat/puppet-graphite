ee862caa802575b296dde2715473edfe67b38eeb
Nathan Sullivan <nsullivan@kixeye.com>
2014-03-20 14:26:16 +1000

    oops missed a spot

--- manifests/params.pp
+++ manifests/params.pp
@@ -64,10 +64,10 @@ class graphite::params {
   $nginxconf_dir = '/etc/nginx/sites-available'
 
   $web_server_pkg = $graphite::gr_web_server ? {
-    apache  => $apache_pkg,
-    nginx   => 'nginx',
-    none    => 'dont-install-webserver-package',
-    default => fail('The only supported web servers are \'apache\', \'nginx\' and \'none\''),
+    apache   => $apache_pkg,
+    nginx    => 'nginx',
+    wsgionly => 'dont-install-webserver-package',
+    default  => fail('The only supported web servers are \'apache\', \'nginx\' and \'wsgionly\''),
   }
 
   $web_user = $::osfamily ? {
