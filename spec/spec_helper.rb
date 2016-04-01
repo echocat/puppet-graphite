863483f9d5b0c7735afcd6b25545b98220e33f04
Daniel Werdermann <Daniel.Werdermann@teufel.de>
2016-04-01 18:13:58 +0200

    add code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+# for code coverage
+
+if ENV['COVERAGE'] == 'yes'
+  require 'coveralls'
+  Coveralls.wear!
+end
+
+RSpec.configure do |c|
+  c.after(:suite) do
+    RSpec::Puppet::Coverage.report!
+  end
+end
